4730cdcaa3a8a716fa1d912b25d67cdbe1f3701f
hortushonu <james.eric.knight@gmail.com>
2019-02-11 13:24:53 +0000

    add contact.html, contact route and contact action in static_pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get     '/portfolio',    to: 'static_pages#portfolio'
+  get     '/portfolio',     to: 'static_pages#portfolio'
+  get     '/contact',       to: 'static_pages#contact'
 end
